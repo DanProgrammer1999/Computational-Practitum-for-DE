2e845bdbdc606f6d2eb129ac155b1ab21682aa83
Daniil Dvoryanov <d.dvoryanov@innopolis.ru>
2018-11-05 03:51:15 +0300

    Implemented a graph for local error. Added documentation to the Calculator class. Done more cleanup.

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,9 @@
 import configparser as cnf
 
 import numpy as np
+from functools import lru_cache
 
 config_file = 'Parameters.ini'
-section = 'CALCULATOR'
-
 
 class Calculator:
     """
@@ -12,22 +11,43 @@ class Calculator:
     in this assignment. It has two separate classes for calculating values and errors,
     and public interface to access the calculations.
     """
+    __section = "CALCULATOR"
 
     def __init__(self):
-        self.__values = self.__Values()
-        self.__errors = self.__Errors(self.__values)
+        """
+        Initialize an object of class Calculator.
+        Create and initialize two instances of each of Calculator's subclasses: Values and Errors.
+        """
+        self.__values = self.Values()
+        self.__errors = self.Errors(self.__values)
 
-    def update(self):
-        self.__values = self.__Values()
-        self.__errors = self.__Errors(self.__values)
-
+    @property
     def x(self):
+        """
+        Get the list of values used by numerical methods for the calculations of the approximated results.
+        :return: A list of values which gives the x axis for the values and local error graphs.
+        """
         return self.__values.x
 
-    def error_steps(self):
+    @property
+    def global_errors_x(self):
+        """
+        Get the list of steps for which the maximum error was calculated.
+        :return: A list of values which gives the x axis for the global (maximum) error graph.
+        """
         return self.__errors.error_x
 
     def get_values(self):
+        """
+        Returns a list of calculates for each approximation method
+        given by the specification of the assignment.
+        :return: A list l of four lists, each consisting of calculation results
+        for specific computational method, namely:
+            l[0] - values for the exact solution;
+            l[1] - values for Euler method;
+            l[2] - values for Improved Euler method;
+            l[3] - values for Runge-Kutta method.
+        """
         exact = self.__values.exact_solution()
         euler = self.__values.euler_method()
         improved_euler = self.__values.improved_euler_method()
@@ -35,18 +55,41 @@ class Calculator:
 
         return [exact, euler, improved_euler, runge_kutta]
 
-    def get_errors(self):
-        euler = self.__errors.euler_error()
-        improved_euler = self.__errors.improved_euler_error()
-        runge_kutta = self.__errors.runge_kutta_error()
+    def get_local_errors(self):
+        """
+        Returns a list of local errors for each approximation method
+        given by the specification of the assignment.
+        :return: A list l of three lists, each consisting of local errors
+        for specific computational method, namely:
+            l[0] - local errors for Euler method;
+            l[1] - local errors for Improved Euler method;
+            l[2] - local errors for Runge-Kutta method.
+        """
+        euler = self.__errors.euler_local()
+        improved_euler = self.__errors.improved_euler_local()
+        runge_kutta = self.__errors.runge_kutta_local()
 
         return [euler, improved_euler, runge_kutta]
 
-    class __Values:
+    def get_global_errors(self):
+        euler = self.__errors.euler_global()
+        improved_euler = self.__errors.improved_euler_global()
+        runge_kutta = self.__errors.runge_kutta_global()
+
+        return [euler, improved_euler, runge_kutta]
+
+    class Values:
+        """
+        A subclass of class Calculator to encapsulate the values-specific calculations.
+        """
 
         __section = "VALUES"
 
         def __init__(self):
+            """
+            Initialize an object of class Calculator.Values.
+            Read all important information from the config and create class variables for it.
+            """
             config = cnf.ConfigParser()
             config.read(config_file)
 
@@ -56,10 +99,13 @@ class Calculator:
             self.__y0 = float(config[self.__section]['y0'])
             self.x = np.arange(self.__x0, self.__xf + self.__h, self.__h)
 
+        @lru_cache(maxsize=1)
         def exact_solution(self, h=None):
             """
-            Calculate an exact solution for each x in the given grid.
-            :param h: Specify a custom step.
+            Calculate an exact solution for the given grid.
+            The "default" result of the function (i.e. for the values and local error graphs) is cached
+            in order to avoid repeating heavy calculations.
+            :param h: Specify a custom step (by default it is specified by the config file).
             :return: A set of y(x) for each x given by the parameters.
             """
             if h is None:
@@ -71,7 +117,16 @@ class Calculator:
 
             return np.array(y)
 
+        @lru_cache(maxsize=1)
         def euler_method(self, h=None):
+            """
+            Calculate an approximated result given by Euler method for the given grid.
+            The "default" result of the function (i.e. for the values and local error graphs) is cached
+            in order to avoid repeating heavy calculations.
+            :param h: Specify a custom step (by default it is specified by the config file).
+            :return: A set of steps calculated by Euler method leading
+            from initial value to final value with step h.
+            """
             if h is None:
                 h = self.__h
                 x = self.x
@@ -85,7 +140,16 @@ class Calculator:
                 y.append(curr_y)
             return np.array(y)
 
+        @lru_cache(maxsize=1)
         def improved_euler_method(self, h=None):
+            """
+            Calculate an approximated result given by Improved Euler method for the given grid.
+            The "default" result of the function (i.e. for the values and local error graphs) is cached
+            in order to avoid repeating heavy calculations.
+            :param h: Specify a custom step (by default it is specified by the config file).
+            :return: A set of steps calculated by Improved Euler method leading
+            from initial value to final value with step h.
+            """
             if h is None:
                 h = self.__h
                 x = self.x
@@ -100,7 +164,16 @@ class Calculator:
                 y.append(curr_y)
             return np.array(y)
 
+        @lru_cache(maxsize=1)
         def runge_kutta_method(self, h=None):
+            """
+            Calculate an approximated result given by Runge-Kutta method for the given grid.
+            The "default" result of the function (i.e. for the values and local error graphs) is cached
+            in order to avoid repeating heavy calculations.
+            :param h: Specify a custom step (by default it is specified by the config file).
+            :return: A set of steps calculated by Runge-Kutta method leading
+            from initial value to final value with step h.
+            """
             if h is None:
                 h = self.__h
                 x = self.x
@@ -123,17 +196,37 @@ class Calculator:
 
         @staticmethod
         def f(x, y):
+            """
+            Calculate a function f(x), such that for the given x and y
+            the result equals to y'(x, y) for the given equation.
+            :param x: value of x.
+            :param y: value of y.
+            :return: y'(x, y), where y' is given by the assignment specifications.
+            """
             return x/y + y/x
 
         @staticmethod
         def exact_value(x):
+            """
+            A formula for the exact solution for the given differential equation.
+            :param x: value of x.
+            :return: y(x), where y is given by the solution to the D.E. specified by the assignment.
+            """
             return x*np.sqrt(2*np.log(x) + 1)
 
-    class __Errors:
+    class Errors:
+        """
+        A subclass of class Calculator to encapsulate the errors-specific calculations.
+        """
 
         __section = "ERRORS"
 
         def __init__(self, values):
+            """
+            Initialize an object of class Calculator.Errors.
+            Read all important information from the config and create class variables for it.
+            :param values (Calculator.__Values): An instance of subclass __Values of class Calculator.
+            """
 
             config = cnf.ConfigParser()
             config.read(config_file)
@@ -151,28 +244,72 @@ class Calculator:
             else:
                 self.error_x = np.arange(self.__error0, self.__errorf + self.__error_step, self.__error_step)
 
-        def euler_error(self):
+        def euler_local(self):
+            """
+            Calculate a list of local errors for the Euler approximation method
+            (the result is given by __local_error function).
+            :return: List of local errors for the Euler method.
+            """
+            return self.__local_error(self.__values.exact_solution(), self.__values.euler_method())
+
+        def improved_euler_local(self):
+            """
+            Calculate a list of local errors for the Improved Euler approximation method
+            (the result is given by __local_error function).
+            :return: List of local errors for the Improved Euler method.
+            """
+            return self.__local_error(self.__values.exact_solution(), self.__values.improved_euler_method())
+
+        def runge_kutta_local(self):
+            """
+            Calculate a list of local errors for the Runge-Kutta approximation method
+            (the result is given by __local_error function).
+            :return: List of local errors for the Runge-Kutta method.
+            """
+            return self.__local_error(self.__values.exact_solution(), self.__values.runge_kutta_method())
+
+        def euler_global(self):
             """
             Calculate a list of maximum local errors for different grid sizes for Improved Euler method.
             :return: A list of maximum local errors for different grid sizes for Improved Euler method.
             """
-            return self.__error(self.__values.euler_method)
+            return self.__global_error(self.__values.euler_method)
 
-        def improved_euler_error(self):
+        def improved_euler_global(self):
             """
             Calculate a list of maximum local errors for different grid sizes for Improved Euler method.
             :return: A list of maximum local errors for different grid sizes for Improved Euler method.
             """
-            return self.__error(self.__values.improved_euler_method)
+            return self.__global_error(self.__values.improved_euler_method)
 
-        def runge_kutta_error(self):
+        def runge_kutta_global(self):
             """
             Calculate a list of maximum local errors for different grid sizes for Runge-Kutta method.
             :return: A list of maximum local errors for different grid sizes for Runge-Kutta method.
             """
-            return self.__error(self.__values.runge_kutta_method)
+            return self.__global_error(self.__values.runge_kutta_method)
 
-        def __error(self, method):
+        @staticmethod
+        def __local_error(exact, y):
+            """
+            Calculate a local error (i.e. a list of values abs(exact[i] - y[i]) for each i).
+            :param exact: The exact solution of the equation for a given grid size.
+            :param y: An approximated solution for a given grid size.
+            :return: A list of local errors for each point of the grid.
+            """
+            return [abs(y[i] - exact[i]) for i in range(len(y))]
+
+        @staticmethod
+        def __maximum_error(exact, y):
+            """
+            Find the maximum local error for the exact and an approximated solution.
+            :param exact: The exact solution of the equation for a given grid size.
+            :param y: An approximated solution for a given grid size.
+            :return: A maximum local error from the list of local errors.
+            """
+            return max(Calculator.Errors.__local_error(exact, y))
+
+        def __global_error(self, method):
             """
             Find a list of maximum local errors for different grid sizes for a given approximation method.
             :param method: Approximation method to find the error of.
@@ -188,22 +325,3 @@ class Calculator:
                 error.append(curr_error)
 
             return np.array(error)
-
-        @staticmethod
-        def __local_error(exact, y):
-            """
-            Calculate a local error, which is just the list of differences between each exact_i and y_i.
-            :param exact: The exact solution of the equation for a given grid size.
-            :param y: An approximated solution for a given grid size.
-            :return: A list of local errors for each point of the grid.
-            """
-            return [abs(y[i] - exact[i]) for i in range(len(y))]
-
-        def __maximum_error(self, exact, y):
-            """
-            Find the maximum local error for the exact and an approximated solution.
-            :param exact: The exact solution of the equation for a given grid size.
-            :param y: An approximated solution for a given grid size.
-            :return: A maximum local error from the list of local errors.
-            """
-            return max(self.__local_error(exact, y))
--- Graph.py
+++ Graph.py
@@ -6,47 +6,48 @@ import plotly.graph_objs as go
 from Calculator import Calculator
 
 config_file = 'Parameters.ini'
-section = 'GRAPH'
-
 
 class Plotter:
 
+    __section = "GRAPH"
     def __init__(self, calculator):
         self.calculator = calculator
         config = cnf.ConfigParser()
         config.read(config_file)
-        self.__values_filename = config[section]['values_filename']
-        self.__errors_filename = config[section]['errors_filename']
-        self.__values_mode = config[section]['values_mode']
-        self.__errors_mode = config[section]['errors_mode']
+        self.__values_filename = config[self.__section]['values_filename']
+        self.__local_errors_filename = config[self.__section]['local_errors_filename']
+        self.__global_errors_filename = config[self.__section]['global_errors_filename']
+        self.__values_mode = config[self.__section]['values_mode']
+        self.__errors_mode = config[self.__section]['errors_mode']
 
     def update(self):
-        self.draw_errors()
-        self.draw_errors()
+        self.draw_global_errors()
+        self.draw_global_errors()
 
     def draw_values(self):
+        x = self.calculator.x
         values = self.calculator.get_values()
 
         go_exact = go.Scatter(
-            x=self.calculator.x(),
+            x=x,
             y=values[0],
             mode=self.__values_mode,
             name='Exact solution')
 
         go_euler = go.Scatter(
-            x=self.calculator.x(),
+            x=x,
             y=values[1],
             mode=self.__values_mode,
             name='Euler method')
 
         go_improved_euler = go.Scatter(
-            x=self.calculator.x(),
+            x=x,
             y=values[2],
             mode=self.__values_mode,
             name='Improved Euler Method')
 
         go_runge_kutta = go.Scatter(
-            x=self.calculator.x(),
+            x=x,
             y=values[3],
             mode=self.__values_mode,
             name='Runge-Kutta method')
@@ -54,9 +55,37 @@ class Plotter:
         data = [go_exact, go_euler, go_improved_euler, go_runge_kutta]
         plotly.offline.plot(data, filename=self.__values_filename, auto_open=False)
 
-    def draw_errors(self):
-        x = self.calculator.error_steps()
-        errors = self.calculator.get_errors()
+    def draw_local_errors(self):
+        x = self.calculator.x
+        errors = self.calculator.get_local_errors()
+
+        go_euler = go.Scatter(
+            x=x,
+            y=errors[0],
+            mode=self.__errors_mode,
+            name='Euler method'
+        )
+
+        go_improved_euler = go.Scatter(
+            x=x,
+            y=errors[1],
+            mode=self.__errors_mode,
+            name='Improved Euler method'
+        )
+
+        go_runge_kutta = go.Scatter(
+            x=x,
+            y=errors[2],
+            mode=self.__errors_mode,
+            name='Runge-Kutta method'
+        )
+
+        data = [go_euler, go_improved_euler, go_runge_kutta]
+        plotly.offline.plot(data, filename=self.__local_errors_filename, auto_open=False)
+
+    def draw_global_errors(self):
+        x = self.calculator.global_errors_x
+        errors = self.calculator.get_global_errors()
 
         go_euler = go.Scatter(
             x=x,
@@ -79,10 +108,11 @@ class Plotter:
             name='Runge-Kutta method'
         )
         data = [go_euler, go_improved_euler, go_runge_kutta]
-        plotly.offline.plot(data, filename=self.__errors_filename, auto_open=False)
+        plotly.offline.plot(data, filename=self.__global_errors_filename, auto_open=False)
 
 
 c = Calculator()
 p = Plotter(c)
 p.draw_values()
-p.draw_errors()
+p.draw_local_errors()
+p.draw_global_errors()
